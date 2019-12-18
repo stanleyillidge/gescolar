902b8b92f27839a339e353ad0b42bed5d98cc83f
Stanley Illidge <stanley.illidge@gmail.com>
2019-12-18 15:27:42 -0500

    login animation

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es';
+// import anime from 'animejs/lib/anime.es';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  
+  constructor() { 
+    // console.log('constr')
+    // this.callAnime()
   }
 
+  ngOnInit() {
+    console.log('ngOnInit')
+    this.callAnime()
+  }
+  
+  callAnime() {
+    anime.timeline({loop: false})
+    .add({
+      targets: '.ml5 .line',
+      opacity: [0.5,1],
+      scaleX: [0, 1],
+      easing: "easeInOutExpo",
+      duration: 700
+    }).add({
+      targets: '.ml5 .line',
+      duration: 600,
+      easing: "easeOutExpo",
+      translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+    }).add({
+      targets: '.ml5 .letters-left',
+      opacity: [0,1],
+      translateX: ["0.5em", 0],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=300'
+    })
+  }
 }

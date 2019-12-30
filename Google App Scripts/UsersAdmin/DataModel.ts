5e2d1f924970a6233fc8208f00b4d05b4af504dd
Stanley Illidge <stanley.illidge@gmail.com>
2019-12-30 01:57:00 -0500

    GAS - v1

--- Google App Scripts/UsersAdmin/DataModel.ts
+++ Google App Scripts/UsersAdmin/DataModel.ts
@@ -1,157 +1,4 @@
-// ---- Basicos -----------------
-    export class Institucion {
-        public key: string;
-        public calendario: 'calendarioA' | 'calendarioB' | 'calendarioC';
-        public razonSocial: string;
-        public rut: string;
-        public nit: string;
-        public escudo: string; // url hacia la img
-        public dane: string;
-        public resolucionAprobacion: string; // url hacia el doc
-        public generoAtendIDo: 'mixto' | 'masculino' | 'femenino';
-        public nivelEnseñanza: {
-            Preescolar: boolean;
-            BasicaPrimaria: boolean;
-            BasicaSecundaria: boolean;
-            EducacionBasicaAdultos: boolean;
-            EducacionMedia: boolean;
-            EducacionMediaAdultos: boolean;
-        };
-        constructor() {
-            this.key = '';
-            this.calendario = 'calendarioA';
-            this.razonSocial = '';
-            this.rut = '';
-            this.nit = '';
-            this.escudo = '';
-            this.dane = '';
-            this.resolucionAprobacion = '';
-            this.generoAtendIDo = 'mixto';
-            this.nivelEnseñanza = {
-            Preescolar: false,
-            BasicaPrimaria: false,
-            BasicaSecundaria: false,
-            EducacionBasicaAdultos: false,
-            EducacionMedia: false,
-            EducacionMediaAdultos: false
-            };
-        }
-    }
-    export class Sedes {
-        public key: string;
-        public nombre: string;
-        public geolocalizacion: {
-            pais: string;
-            departamento: string;
-            municipio: string;
-            direccion: string;
-            telefono: number;
-            coordenadas: {
-            latitud: number;
-            longitud: number;
-            };
-        };
-        public dane: string;
-        public jornadas: {
-            mañana: boolean;
-            tarde: boolean;
-            nocturna: boolean;
-            sabatina: boolean;
-            unica: boolean;
-        };
-        constructor() {
-            this.key = '';
-            this.nombre = '';
-            this.geolocalizacion = {
-            pais: '',
-            departamento: '',
-            municipio: '',
-            direccion: '',
-            telefono: 0,
-            coordenadas: {
-                latitud: 0,
-                longitud: 0
-            }
-            };
-            this.dane = '';
-            this.jornadas = {
-            mañana: false,
-            tarde: false,
-            nocturna: false,
-            sabatina: false,
-            unica: false
-            };
-        }
-    }
-// ---- G-suite User ------------
-    // https://developers.google.com/admin-sdk/directory/v1/reference/users#resource
-    /*event:{
-        name: {
-            familyName: picho,
-            givenName: perez
-        },
-        password: 123456789,
-        primaryEmail: picho@lreginaldofischione.edu.co,
-        organizations: [{
-            title: Directivo(a) Docente,
-            primary: true,
-            customType: ,
-            description: Coordinador
-        }],
-        // nuevos
-        includeInGlobalAddressList: boolean, Indica si el perfil del usuario está visible
-        orgUnitPath: string, La ruta completa de la organización principal se representa ( /).
-        organizations: [{ // Lista de organizaciones El tamaño máximo es de 10Kb.
-            name: string, // El nombre de la organización.
-            title: string, // El título del usuario dentro de la organización, por ejemplo, 'miembro' o 'ingeniero'.
-            primary: boolean, // Indica si esta es la organización principal del usuario.
-            type: string, // El tipo de organización. Los valores aceptables son:  domain_only |  school |  unknown |  work
-            customType: string, // Si el valor de type es custom, esta propiedad contiene el tipo personalizado.
-            department: string, // Especifica el departamento dentro de la organización, como 'ventas' o 'ingeniería'
-            symbol: string, // Símbolo de cadena de texto de la organización. Por ejemplo, el símbolo de texto para Google es GOOG.
-            location: string, // La ubicación física de la organización.
-            description: string, // La descripción de la organización.
-            domain: string, // El dominio al que pertenece la organización.
-            costCenter: string,
-            fullTimeEquivalent: integer // El milipercent equivalente a tiempo completo dentro de la organización (100000 = 100%).
-        }],
-        relations: [{ // Una lista de las relaciones del usuario con otros usuarios. El tamaño máximo es de 2Kb.
-            value: string, // El nombre de la persona con la que está relacionado el usuario
-            type: string,
-            customType: string // Si el valor de type es custom, esta propiedad contiene el tipo personalizado.
-        }],
-        addresses: [{ // A list of the user's addresses. Maximum allowed data size for this field is 10Kb.
-            type: string, // custom | home | other | work
-            customType: string, // Si la dirección type es custom, esta propiedad contiene el valor personalizado.
-            sourceIsStructured: boolean,
-            formatted: string,
-            poBox: string, // El apartado de correos, si está presente.
-            extendedAddress: string, // barrio
-            streetAddress: string, // La dirección
-            locality: string, // El pueblo o ciudad de la dirección.
-            region: string, // La provincia o estado abreviado.
-            postalCode: string, // El código postal o postal, si corresponde.
-            country: string, // País.
-            primary: boolean, // Si esta es la dirección principal del usuario
-            countryCode: string // El código del país Utiliza el estándar ISO 3166-1 .
-        }],
-        phones: [{ // Una lista de los números de teléfono del usuario. El tamaño máximo es de 1Kb.
-            value: string, // Un número de teléfono legible para humanos. Puede estar en cualquier formato de número de teléfono.
-            primary: boolean, // Indica si este es el número de teléfono principal del usuario
-            type: string,
-            customType: string // Si el valor de type es custom, esta propiedad contiene el tipo personalizado.
-        }],
-        gender: { // El tamaño máximo es de 1Kb.
-            type: string, //  female |  male |  other |  unknown
-            customGender: string, // Género personalizado.
-            addressMeAs: string // forma correcta de referirse al propietario, por ejemplo él / él / su o ellos / ellos / su.
-        },
-        customSchemas: { // objetos relacionados varios ejem: grados, grupos, etc
-            (key): {
-                (key): (value)
-            }
-        }
-    } */
+// ---- Data Models ------------
     type roles =
         | 'super'
         | 'rector'
@@ -174,7 +21,7 @@
         constructor(a) {
             this.familyName = a.familyName;
             this.givenName = a.givenName;
-            this.fullName = ((a.fullName) ? a.fullName : '');
+            this.fullName = ((a.familyName) ? (a.givenName + ' ' + a.familyName) : '');
         }
     }
     export class GsuiteOrganizations {
@@ -226,7 +73,7 @@
         country: string; // País.
         primary: boolean; // Si esta es la dirección principal del usuario.
         constructor(a) {
-            this.type = a.type;
+            this.type = 'home';
             this.streetAddress = a.streetAddress;
             this.extendedAddress = a.extendedAddress;
             this.locality = a.locality;
@@ -295,6 +142,7 @@
             this.grupo = ((a.grupo) ? a.grupo : '');
         }
     }
+// -----------------------------
     export class GsuiteUser {
         // adaptador para crear usuarios con directory api de g-suite
         public id?: string;
@@ -303,14 +151,13 @@
         public primaryEmail: string;
         public organizations: GsuiteOrganizations[];
         public orgUnitPath: string; // sede, creadas prev en las unid organizativas de g suite
-        includeInGlobalAddressList?: boolean;
         public relations?: GsuiteRelations[];
         public addresses: GsuiteAddresses[];
         public phones?: GsuitePhones[];
         public gender: GsuiteGender;
         public customSchemas: CustomSchemas;
+        includeInGlobalAddressList?: boolean;
         constructor(user: any) {
-            this.id = ((user.id) ? user.id : '');
             this.name = user.name;
             this.password = user.password;
             this.primaryEmail = user.primaryEmail;
@@ -334,7 +181,6 @@
             return age;
         }
     }
-// ---- Firebase user -----------
     export class Relaciones extends GsuiteRelations {
         fecha: Date; // fecha de creacion
         uid: string; // uid del usuario 'acudiente' del 'estudiante'
@@ -411,7 +257,6 @@
             return age;
         }
     }
-// ------------------------------
     export class AñoLectivo {
         public key: string;
         public inicio: Date;
@@ -436,18 +281,6 @@
             this.acudientes = acud;
             this.grado = g;
             this.grupo = gr;
-        }
-    }
-    export class GestionDocumental {
-        public key: string;
-        constructor() {
-            this.key = '';
-        }
-    }
-// ---- Data Base ---------------
-    export class LocalDatabase {
-        // public Matriculas: { [key: string]: Matricula };
-        constructor() {
         }
     }
 // ------------------------------
---
+++ Google App Scripts/UsersAdmin/code.ts
@@ -0,0 +1,173 @@
+// --- Data Models ------------------
+    import { GsuiteName, GsuiteUser, GsuiteOrganizations } from './DataModel';
+    import { GsuiteRelations, GsuiteAddresses, GsuitePhones } from './DataModel';
+    import { GsuiteGender, CustomSchemas } from './DataModel';
+// --- Global Variables -------------
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Estudiantes');
+    const data = sheet.getDataRange().getValues();
+    const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues();
+
+    // --- Colmunas a tener en cuenta ---
+    const Apellido1Index = headers[0].indexOf('Apellido1');
+    const Apellido2Index = headers[0].indexOf('Apellido2');
+    const Nombre1Index = headers[0].indexOf('Nombre1');
+    const Nombre2Index = headers[0].indexOf('Nombre2');
+    const FechaNacimientoIndex = headers[0].indexOf('Fecha de nacimiento');
+    const EmailIndex = headers[0].indexOf('Email');
+    const PasswordIndex = headers[0].indexOf('Password');
+    const RolIndex = headers[0].indexOf('Rol');
+    const DocTipoIndex = headers[0].indexOf('Doc Tipo');
+    const DocNumIndex = headers[0].indexOf('Doc Num');
+    const SedeIndex = headers[0].indexOf('Sede');
+    const JornadaIndex = headers[0].indexOf('Jornada');
+    const GradoIndex = headers[0].indexOf('Grado');
+    const GrupoIndex = headers[0].indexOf('Grupo');
+    const DireccionIndex = headers[0].indexOf('Direccion');
+    const BarrioIndex = headers[0].indexOf('Barrio');
+    const MunicipioIndex = headers[0].indexOf('Municipio');
+    const DepartamentoIndex = headers[0].indexOf('Departamento');
+    const GeneroIndex = headers[0].indexOf('Genero');
+    const ACApellido1Index = headers[0].indexOf('AC-Apellido1');
+    const ACApellido2Index = headers[0].indexOf('AC-Apellido2');
+    const ACNombre1Index = headers[0].indexOf('AC-Nombre1');
+    const ACNombre2Index = headers[0].indexOf('AC-Nombre2');
+    const ACFechaNacimientoIndex = headers[0].indexOf('AC-Fecha de nacimiento');
+    const ACEmailIndex = headers[0].indexOf('AC-Email');
+    const ACPasswordIndex = headers[0].indexOf('AC-Password');
+    const ACRolIndex = headers[0].indexOf('AC-Rol');
+    const ACParentezcoIndex = headers[0].indexOf('AC-Parentezco');
+    const ACDocTipoIndex = headers[0].indexOf('AC-Doc Tipo');
+    const ACDocNumIndex = headers[0].indexOf('AC-Doc Num');
+    const ACTelefonoIndex = headers[0].indexOf('AC-Telefono');
+    const ACDireccionIndex = headers[0].indexOf('AC-Direccion');
+    const ACBarrioIndex = headers[0].indexOf('AC-Barrio');
+    const ACMunicipioIndex = headers[0].indexOf('AC-Municipio');
+    const ACDepartamentoIndex = headers[0].indexOf('AC-Departamento');
+    const ACGeneroIndex = headers[0].indexOf('AC-Genero');
+// --------------------------------
+    function onOpen() {
+        const ui = SpreadsheetApp.getUi();
+        ui.createMenu('Administracion de Usuarios')
+            .addItem('Crear usuarios', 'menuItem1')
+            .addSeparator()
+            .addSubMenu(ui.createMenu('Cargas')
+                .addItem('Carga consolidado', 'showPicker'))
+            .addToUi();
+    }
+
+    function menuItem1() {
+        for (const i in data) {
+            if (data.hasOwnProperty(i)) {
+                const a = new GsuiteUser({
+                    name: new GsuiteName({
+                        familyName: data[i][Apellido1Index],
+                        givenName: data[i][Nombre1Index]
+                    }),
+                    password: data[i][PasswordIndex],
+                    primaryEmail: data[i][EmailIndex],
+                    organizations: new GsuiteOrganizations({
+                        title: data[i][RolIndex]
+                    }),
+                    orgUnitPath: data[i][SedeIndex],
+                    relations: new GsuiteRelations({
+                        value: data[i][ACNombre1Index] + ' ' + data[i][ACApellido1Index],
+                        type: 'father'
+                    }),
+                    addresses: new GsuiteAddresses({
+                        streetAddress: data[i][DireccionIndex],
+                        extendedAddress: data[i][BarrioIndex],
+                        locality: data[i][DepartamentoIndex],
+                        region: data[i][MunicipioIndex]
+                    }),
+                    phones: new GsuitePhones({
+                        value: data[i][ACTelefonoIndex]
+                    }),
+                    gender: new GsuiteGender({
+                        type: ((data[i][GeneroIndex] === 'masculino') ? 'male' : 'female')
+                    }),
+                    customSchemas: new CustomSchemas({
+                        Identificacion: {
+                            tipo: data[i][DocTipoIndex],
+                            numero: data[i][DocNumIndex],
+                            fechaNacim: data[i][FechaNacimientoIndex]
+                        },
+                        jornada: data[i][JornadaIndex],
+                        grado: data[i][GradoIndex],
+                        grupo: data[i][GrupoIndex]
+                    }),
+                });
+                Logger.log('Usuario ' + i);
+                Logger.log(a);
+            }
+        }
+        SpreadsheetApp.getUi()
+            .alert('You clicked the first menu item! - Sta Ok - 4');
+    }
+
+    function menuItem2() {
+        SpreadsheetApp.getUi() // Or DocumentApp or FormApp.
+            .alert('You clicked the second menu item!');
+    }
+
+    function showPicker() {
+        const html = HtmlService.createHtmlOutputFromFile('piker.html')
+        .setWidth(600)
+        .setHeight(425)
+        .setSandboxMode(HtmlService.SandboxMode.IFRAME);
+        SpreadsheetApp.getUi().showModalDialog(html, 'Select a file');
+    }
+
+    function createDialog() {
+        const htmlOutput = HtmlService
+            .createHtmlOutputFromFile('dialog')
+            .setWidth(250)
+            .setHeight(80);
+        SpreadsheetApp.getUi().showModalDialog(htmlOutput, 'Loading');
+    }
+
+    function getOAuthToken() {
+        DriveApp.getRootFolder();
+        return ScriptApp.getOAuthToken();
+    }
+
+    function listFilesInFolder() {
+        const file = DriveApp.getFolderById('1lmHADksYPYrvNITE0Z0M9BpkAdDg31q9'); // Folder "Notas"
+        const files = file.getFiles();
+        while (files.hasNext()) {
+            const File = files.next();
+            Logger.log(File.getName() + ' ' + File.getId());
+            //    var fBlob = DriveApp.getFileById(File.getId()).getBlob();
+            //    var File = {
+            //      title: File.getName(),
+            //      mimeType: 'application/vnd.google-apps.spreadsheet'
+            //    };
+            //    File = Drive.Files.insert(File, fBlob);
+            const consolidado = SpreadsheetApp.openById(File.getId());
+            const ConsolidadoSheets = consolidado.getSheets();
+            // --- Estructura de los datos -----------
+            const ConsolidadoLastRow = ConsolidadoSheets[0].getLastRow();
+            const ConsolidadoLastColumn = ConsolidadoSheets[0].getLastColumn();
+            const ConsolidadoRange  = ConsolidadoSheets[0].getRange(1, 1, ConsolidadoLastRow, ConsolidadoLastColumn);
+            const ConsolidadoValues = ConsolidadoRange.getValues();
+            for (let i =  0; i < ConsolidadoLastRow; i++) {
+                const nombre = ConsolidadoValues[i][3].toString();
+                if (nombre !== '') {
+                    let c = 0;
+                    const notas = new Array(9);
+                    for (let j = 12; j < 21; j++) { // numero de asignaturas 9 + inicio que es la coumna 12
+                        notas[c] = [ConsolidadoValues[i][j], ConsolidadoValues[i + 1][j]];
+                        c = c + 1;
+                    }
+                    Logger.log(nombre + ' ' + notas);
+                }
+            }
+            // ---------------------------------------
+            //    for(var i =  0; i < sheets.length; i++){
+            //      var range  = sheets[i].getRange(1, 1, sheets[i].getLastRow(), sheets[i].getLastColumn());
+            //      var values = range.getValues();
+            //      for(var j = 0; j < sheets[i].getLastRow(); j++){
+            //        Logger.log(values[j][0].toString());
+            //      }
+            //    }
+        }
+    }

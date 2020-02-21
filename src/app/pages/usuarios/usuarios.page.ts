92a16e8c6b6e2804591947c0bc680ebd41ab3643
Stanley Illidge <stanley.illidge@gmail.com>
2020-02-21 12:01:38 -0500

    users mang v2

--- src/app/pages/usuarios/usuarios.page.ts
+++ src/app/pages/usuarios/usuarios.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { ActionSheetController, Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/AuthService';
 import { DataService2 } from 'src/app/services/data-service';
@@ -8,12 +8,12 @@ import { GescolarUser } from 'src/app/models/data-models';
 import {NestedTreeControl} from '@angular/cdk/tree';
 import {MatTreeNestedDataSource} from '@angular/material/tree';
 
-/* interface Node {
-  name: string;
-  checked: boolean;
-  indeterminate: boolean;
-  children?: Node[];
-} */
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {SelectionModel} from '@angular/cdk/collections';
+
+// --- Tree component -------
 export class Estado {
   ruta: any;
   keys: any;
@@ -29,105 +29,26 @@ export class Node2 {
   estado: Estado;
   children?: Node2[];
 }
-/* const TREE_DATA: Node[] = [
-  {
-    name: 'Fruit',
-    checked: false,
-    indeterminate: false,
-    children: [
-      {name: 'Apple',
-      checked: false,
-      indeterminate: false},
-      {name: 'Banana',
-      checked: false,
-      indeterminate: false},
-      {name: 'Fruit loops',
-      checked: false,
-      indeterminate: false},
-    ]
-  }, {
-    name: 'Vegetables',
-    checked: false,
-    indeterminate: false,
-    children: [
-      {
-        name: 'Green',
-        checked: false,
-        indeterminate: false,
-        children: [
-          {name: 'Broccoli',
-          checked: false,
-          indeterminate: false},
-          {name: 'Brussels sprouts',
-          checked: false,
-          indeterminate: false},
-        ]
-      }, {
-        name: 'Orange',
-        checked: false,
-        indeterminate: false,
-        children: [
-          {name: 'Pumpkins',
-          checked: false,
-          indeterminate: false},
-          {name: 'Carrots',
-          checked: false,
-          indeterminate: false},
-        ]
-      },
-    ]
-  },
-]; */
-/* {
-  'Fruit',
-  {
-    {'Apple'},
-    {'Banana'},
-    {'Fruit loops'},
+// --- Table ----------------
+export class UserDataTable {
+  nombre: string;
+  email: string;
+  cargo: string;
+  telefono: string;
+  position: number;
+  constructor(a: any, pos: number) {
+    this.nombre = a.nombre;
+    this.email = a.email;
+    this.cargo = a.rol;
+    this.telefono = a.telefonos[0].value;
+    this.position = pos;
   }
-}, {
-  'Vegetables',
-  {
-    {
-      'Green',
-      {
-        {'Broccoli'},
-        {'Brussels sprouts'},
-      }
-    }, {
-      'Orange',
-      {
-        {'Pumpkins'},
-        {'Carrots'},
-      }
-    },
-  }
-}, */
-const TREE_DATA2 = {
-  Fruits: {
-    Apple: null,
-    Berries: {
-      'Blueberry': null,
-      'Raspberry': ['fresas', 'moras']
-    },
-    Orange: null
-  },
-  Vegetables: {
-    Greens: ['Broccoli', 'Brussels sprouts'],
-    Oranges: {'Pumpkins': null, 'Carrots': null}
-  },
-  Groceries: {
-    'Almond Meal flour': null,
-    'Organic eggs': null,
-    'Protein Powder': null,
-    'Fruit loops': {
-      Apple: null,
-      Berries: ['Blueberry', 'Raspberry'],
-      Orange: null
-    }
-  }
-};
-
+}
+export interface Section {
+  name: string;
+  updated: Date;
+}
+// --------------------------
 
 @Component({
   selector: 'app-usuarios',
@@ -137,28 +58,141 @@ const TREE_DATA2 = {
 export class UsuariosPage implements OnInit {
   data: any;
   usuarios: any;
-  treeControl = new NestedTreeControl<Node2>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<Node2>();
-  nodo = 0;
-  ruta: any = [];
-  nivel = 0;
-  keys: any[];
-  loop = 0;
+  // --- tree ----------------------
+    treeControl = new NestedTreeControl<Node2>(node => node.children);
+    treeData = new MatTreeNestedDataSource<Node2>();
+    nodo = 0;
+    ruta: any = [];
+    nivel = 0;
+    keys: any[];
+    loop = 0;
+  // --- Table ---------------------
+    displayedColumns: string[] = ['select', 'nombre', 'email', 'cargo', 'telefono'];
+    displayedColumnsT: string[] = ['select', 'nombre', 'email', 'cargo', 'telefono'];
+    tableData: MatTableDataSource<UserDataTable>;
+    selection = new SelectionModel<UserDataTable>(true, []);
+    @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+    @ViewChild(MatSort, {static: true}) sort: MatSort;
+    users: any[];
+    myInnerHeight = 0;
+    states = [
+      {name: 'Alabama', capital: 'Montgomery'},
+      {name: 'Alaska', capital: 'Juneau'},
+      {name: 'Arizona', capital: 'Phoenix'},
+      {name: 'Arkansas', capital: 'Little Rock'},
+      {name: 'California', capital: 'Sacramento'},
+      {name: 'Colorado', capital: 'Denver'},
+      {name: 'Connecticut', capital: 'Hartford'},
+      {name: 'Delaware', capital: 'Dover'},
+      {name: 'Florida', capital: 'Tallahassee'},
+      {name: 'Georgia', capital: 'Atlanta'},
+      {name: 'Hawaii', capital: 'Honolulu'},
+      {name: 'Idaho', capital: 'Boise'},
+      {name: 'Illinois', capital: 'Springfield'},
+      {name: 'Indiana', capital: 'Indianapolis'},
+      {name: 'Iowa', capital: 'Des Moines'},
+      {name: 'Kansas', capital: 'Topeka'},
+      {name: 'Kentucky', capital: 'Frankfort'},
+      {name: 'Louisiana', capital: 'Baton Rouge'},
+      {name: 'Maine', capital: 'Augusta'},
+      {name: 'Maryland', capital: 'Annapolis'},
+      {name: 'Massachusetts', capital: 'Boston'},
+      {name: 'Michigan', capital: 'Lansing'},
+      {name: 'Minnesota', capital: 'St. Paul'},
+      {name: 'Mississippi', capital: 'Jackson'},
+      {name: 'Missouri', capital: 'Jefferson City'},
+      {name: 'Montana', capital: 'Helena'},
+      {name: 'Nebraska', capital: 'Lincoln'},
+      {name: 'Nevada', capital: 'Carson City'},
+      {name: 'New Hampshire', capital: 'Concord'},
+      {name: 'New Jersey', capital: 'Trenton'},
+      {name: 'New Mexico', capital: 'Santa Fe'},
+      {name: 'New York', capital: 'Albany'},
+      {name: 'North Carolina', capital: 'Raleigh'},
+      {name: 'North Dakota', capital: 'Bismarck'},
+      {name: 'Ohio', capital: 'Columbus'},
+      {name: 'Oklahoma', capital: 'Oklahoma City'},
+      {name: 'Oregon', capital: 'Salem'},
+      {name: 'Pennsylvania', capital: 'Harrisburg'},
+      {name: 'Rhode Island', capital: 'Providence'},
+      {name: 'South Carolina', capital: 'Columbia'},
+      {name: 'South Dakota', capital: 'Pierre'},
+      {name: 'Tennessee', capital: 'Nashville'},
+      {name: 'Texas', capital: 'Austin'},
+      {name: 'Utah', capital: 'Salt Lake City'},
+      {name: 'Vermont', capital: 'Montpelier'},
+      {name: 'Virginia', capital: 'Richmond'},
+      {name: 'Washington', capital: 'Olympia'},
+      {name: 'West Virginia', capital: 'Charleston'},
+      {name: 'Wisconsin', capital: 'Madison'},
+      {name: 'Wyoming', capital: 'Cheyenne'},
+    ];
+  // -------------------------------
+  plataforma = {desktop: false, android: false, cordova: false};
+  myInnerWidth: number;
   constructor(
+    public platform: Platform,
     public router: Router,
     public ds: DataService2,
     public authService: AuthService,
     public actionSheetController: ActionSheetController
-  ) {
-    // console.log(this.buildFileTree(TREE_DATA2, 0));
+  ) {}
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    // this.screenHeight = window.innerHeight;
+    // this.screenWidth = window.innerWidth;
+    const windowsize = ((event) ? event.target.innerWidth : window.innerWidth);
+    this.myInnerWidth = (window.innerWidth * 0.015);
+    console.log(windowsize);
+    console.log(this.plataforma);
+    console.log(this.myInnerWidth, this.myInnerHeight);
+    // console.log(this.displayedColumnsT);
+    const a = [];
+    this.displayedColumnsT.forEach(element => {
+      a.push(element);
+    });
+    this.displayedColumns = a;
+    if (windowsize > 300 && windowsize < 500) {
+      this.plataforma.android = true;
+      this.plataforma.cordova = true;
+      this.plataforma.desktop = false;
+      this.myInnerHeight = window.innerHeight - (window.innerHeight * 0.27);
+      console.log(this.users);
+      // this.hideColum('telefono');
+      // this.hideColum('email');
+      // this.hideColum('cargo');
+    } else if (windowsize > 499 && windowsize < 800) {
+      this.plataforma.android = false;
+      this.plataforma.cordova = false;
+      this.plataforma.desktop = true;
+      this.hideColum('telefono');
+      this.hideColum('email');
+    } else if (windowsize > 799 && windowsize < 900) {
+      this.plataforma.android = false;
+      this.plataforma.cordova = false;
+      this.plataforma.desktop = true;
+      this.hideColum('telefono');
+    }
+  }
+  hideColum(label: string) {
+    const index = this.displayedColumns.indexOf(label);
+    if (index > -1) {
+      this.displayedColumns.splice(index, 1);
+    }
   }
   viewUser(page: string, i: number, edit: boolean) {
     console.log(page, i, edit);
     this.router.navigate([page, { id: i }]);
   }
   hasChild = (_: number, node: Node2) => !!node.children && node.children.length > 0;
-  async ngOnInit() {
+
+  ngOnInit() {
     const este = this;
+    this.myInnerHeight = window.innerHeight - (window.innerHeight * 0.27);
+    this.myInnerWidth = (window.innerWidth * 0.015);
+    this.plataforma.desktop = this.platform.is('desktop');
+    this.plataforma.android = this.platform.is('android');
+    this.plataforma.cordova = this.platform.is('cordova');
     let user;
     this.ds.getUser().then((u) => {
       user = new GescolarUser(u);
@@ -166,13 +200,25 @@ export class UsuariosPage implements OnInit {
     // console.log(user, este.ds.database);
     // obtengo las sedes de la base de datos
     this.data = {Institucion:[]}; // defino el objeto sedes para cargar en el menu de arbol
+    this.users = [];
     for (const i in this.ds.database.sedes) {
       if (this.ds.database.sedes.hasOwnProperty(i)) {
         this.data.Institucion.push(this.ds.database.sedes[i].nombre);
       }
     }
-    this.dataSource.data = this.buildFileTree(this.data, 0);
-    console.log(this.dataSource.data);
+    this.treeData.data = this.buildFileTree(this.data, 0);
+    let conta = 0;
+    for (const i in this.ds.database.usuarios) {
+      if (this.ds.database.usuarios.hasOwnProperty(i)) {
+        this.users.push(new UserDataTable(this.ds.database.usuarios[i], conta));
+        conta = conta + 1;
+      }
+    }
+    this.tableData = new MatTableDataSource(this.users);
+    this.tableData.paginator = this.paginator;
+    this.tableData.sort = this.sort;
+    console.log(this.treeData.data, this.tableData);
+    this.onResize();
     // verifico la existencia de los datos necesarios localmente o en internet si no estan locales
     // si no estan en ninguno de los medios, muestro el formulario vacio
     if (!this.ds.database.usuarios) {
@@ -190,10 +236,42 @@ export class UsuariosPage implements OnInit {
       });
     }
   }
-  // --- tree component ---------------
+  // --- Table component --------------
+    applyFilter(event: Event) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.tableData.filter = filterValue.trim().toLowerCase();
+
+      if (this.tableData.paginator) {
+        this.tableData.paginator.firstPage();
+      }
+    }
+    /** Whether the number of selected elements matches the total number of rows. */
+    isAllSelected() {
+      const numSelected = this.selection.selected.length;
+      const numRows = this.tableData.data.length;
+      return numSelected === numRows;
+    }
+    /** Selects all rows if they are not all selected; otherwise clear selection. */
+    masterToggle() {
+      this.isAllSelected() ?
+          this.selection.clear() :
+          this.tableData.data.forEach(row => this.selection.select(row));
+    }
+    /** The label for the checkbox on the passed row */
+    checkboxLabel(row?: UserDataTable): string {
+      if (!row) {
+        return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+      }
+      return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    }
+    rowSelect(row: any) {
+      console.log(row);
+      this.selection.toggle(row);
+    }
+  // --- Tree component ---------------
     onChange(node: Node2) {
       console.log('onChange', node);
-      const nodos = this.dataSource.data;
+      const nodos = this.treeData.data;
       const keys = ((node.estado.keys.length > 0) ? node.estado.keys : [node.estado.item]);
       this.loop = node.estado.keys.length - 1;
       this.deepNode(node, node.estado.checked);
@@ -214,7 +292,7 @@ export class UsuariosPage implements OnInit {
           this.accion(node, t);
           this.loop = this.loop - 1;
           if (this.loop === -1) { return; }
-          this.iteraNodos(this.dataSource.data[keys[0]], keys, 0, checked);
+          this.iteraNodos(this.treeData.data[keys[0]], keys, 0, checked);
         });
       } else if (node.children[keys[j]]) {
         this.iteraNodos(node.children[keys[j]], keys, j, checked);
